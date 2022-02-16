35a7f5a56707beadcb7d95bd842eae9d6aa33db7
Connor Bremner <46701830+JohnathanPizza@users.noreply.github.com>
2022-02-15 20:43:25 -0600

    Update contour_detection.py
    clean

--- src/vision_targeting/src/contour_detection.py
+++ src/vision_targeting/src/contour_detection.py
@@ -9,21 +9,20 @@ class ContourDetection:
     def getContours(self):
         mask = self.mask
         height, width = mask.shape
-        edges=None
+        edges = None
         # for i in range(3,7, 2):
-        edges = cv2.Canny(mask,100, 150, apertureSize=5)
+        edges = cv2.Canny(mask, 100, 150, apertureSize = 5)
         #cv2.imshow("Canny", edges)
         contours, _= cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         newContours = []
         for contour in contours:
             if cv2.contourArea(contour)>self.minArea:
-                if 3<self.getSideNumber(contour)<8:
+                if 3 < self.getSideNumber(contour) < 8:
                     newContours.append(contour)
-
         return newContours
 
     def getSideNumber(self, contour):
-        epilson = 0.04*cv2.arcLength(contour, True)
+        epilson = 0.04 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epilson, True)
         return len(approx)
     
