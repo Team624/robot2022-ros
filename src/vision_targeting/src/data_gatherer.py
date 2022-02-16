6f9b750199413a6d9e06ee94f34e5a4d9d3694ae
Connor Bremner <46701830+JohnathanPizza@users.noreply.github.com>
2022-02-15 20:37:52 -0600

    clean up
    removed getDistanceBetweenTwoPoints because its a standard python function (math.dist)
    put in math.degrees to get degrees from radians from math.atan2
    spacing between operators

--- src/vision_targeting/src/data_gatherer.py
+++ src/vision_targeting/src/data_gatherer.py
@@ -6,7 +6,7 @@ import rospy
 class DataGatherer:
     def __init__(self, image):
         self.contours = contour_detection.ContourDetection(image).getContours()
-        self.w, self.h =image.shape
+        self.w, self.h = image.shape
 
     def getCenters(self):
         mainCountors = self.contours
@@ -17,69 +17,60 @@ class DataGatherer:
             cY = int(M["m01"] / M["m00"])
             points.append((cX, cY, c))
         return points
-
-    def getDistanceBetweenTwoPoints(self, p1, p2):
-        x1, y1 = p1[0], p1[1]
-        x2, y2 = p2[0], p2[1]
-        return abs((x1-x2)**2+(y1-y2)**2)**.5
+    
     def getAverageCenter(self):
         self.contours = self.getBestContours(rospy.get_param("~tolerance", 40))
-        if len(self.contours)<3:
+        if len(self.contours) < 3:
             return None
-        totalX=0
-        totalY=0
-        for (x,y,c) in self.getCenters():
-            totalX=totalX+x
-            totalY=totalY+y
+        totalX = 0
+        totalY = 0
+        for (x, y, c) in self.getCenters():
+            totalX += x
+            totalY += y
         length = len(self.contours)
-        return [totalX/length, totalY/length]
+        return [totalX / length, totalY / length]
 
     def get_x_offset(self, x, wanted_x):
-        return x - (wanted_x)
+        return x - wanted_x
 
     def inRange(self, x, minX, maxX):
-        return minX<=x and x<=maxX
+        return minX <= x and x <= maxX
 
     def getBestContours(self, tolerance):
-        variation = self.h/tolerance
+        variation = self.h / tolerance
         data, newData = [], []
         centers = self.getCenters()
         for i in range(0, len(centers)):
             mainCntY = centers[i][1]
             for cnt in centers:
-                if self.inRange(cnt[1], mainCntY-variation, mainCntY+variation):
+                if self.inRange(cnt[1], mainCntY - variation, mainCntY + variation):
                     newData.append(cnt[2])
-            if len(newData)>len(data):
+            if len(newData) > len(data):
                 data = newData
-            newData=[]
+            newData = []
         
-        if len(data)<3:
+        if len(data) < 3:
             return []
         
         data = self.getCenterX(data)
         data = sorted(data, key = lambda x: x[0])
         distances = []
-        for i in range(0,len(data)-1):
-            distances.append(self.getDistanceBetweenTwoPoints(
-                [data[i][0],0],
-                [data[i+1][0], 0],
-            ))
-        average = sum(distances)/len(distances)
+        for i in range(0, len(data) - 1):
+            distances.append(math.dist([data[i][0]], [data[i + 1][0]]))
+        average = sum(distances) / len(distances)
         maxdist = max(distances)
-        if maxdist/average>1.4:
+        if maxdist / average > 1.4:
             index = distances.index(maxdist)
-            if index>len(distances)/2:
-                data = data[0:index+1]
-            elif index<len(distances)/2:
-                data=data[index+1:]
+            if index > len(distances) / 2:
+                data = data[0 : index + 1]
+            elif index < len(distances) / 2:
+                data = data[index + 1 :]
             else:
                 return []
         for i in range(0, len(data)):
-
-           data[i]=data[i][2]
-        if len(data)<3:
+           data[i] = data[i][2]
+        if len(data) < 3:
             return [] 
-
         return data
         
     def getCenterX(self, contours):
@@ -92,41 +83,34 @@ class DataGatherer:
             points.append((cX, cY, c))
         return points
 
-
-
-
     def getExtremes(self):
         data = self.getCenters()
         newData = []
         for i in range(0, len(data)):
-            newData.append(
-                [
-                    data[i][0],
-                    data[i][1],
-                    self.contours[i]
-                ]
-
-        )
-        data=newData
-        data = sorted(data, key=lambda x2: x2[0])
-        c=data[0][2]
+            newData.append([
+                data[i][0],
+                data[i][1],
+                self.contours[i]
+            })
+        data = sorted(newData, key = lambda x2: x2[0])
+        c = data[0][2]
         left = tuple(c[c[:, :, 0].argmin()][0])
-        c=data[-1][2]
-        right=tuple(c[c[:, :, 0].argmax()][0])
+        c = data[-1][2]
+        right = tuple(c[c[:, :, 0].argmax()][0])
         return left, right
 
     def getAngle(self, averageCenter, center_x, height):
-        angle = (math.atan2(self.get_x_offset(averageCenter[0], center_x),  height))*180/math.pi % 360.0
-        if angle>180:
-            return 360-angle
+        angle = math.degrees(math.atan2(self.get_x_offset(averageCenter[0], center_x),  height))
+        if angle > 180:
+            return 360 - angle
         else:
             return angle
     
     def getAngleAdvanced(self, averageCenter, center_x, height, width):
-        angle = (math.atan2(self.get_x_offset(averageCenter[0], center_x),  height))*180/math.pi % 360.0
-        if angle>180:
-            angle= 360-angle
-        if averageCenter[0]>=width/2:
+        angle = math.degrees(math.atan2(self.get_x_offset(averageCenter[0], center_x),  height))
+        if angle > 180:
+            angle = 360 - angle
+        if averageCenter[0] >= width / 2:
             return angle
         else:
             return -angle
