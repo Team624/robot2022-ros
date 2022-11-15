9e2e0913fff19b681954a7bbcb36d6a230865643
kartikr27 <krishsuper27@gmail.com>
2022-11-14 23:36:10 -0600

    formatting for public release

--- src/vision_targeting/src/color_filter.py
+++ src/vision_targeting/src/color_filter.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import rospy
+
+
 class ColorFilter:
 
     def __init__(self, frame):
@@ -11,5 +13,5 @@ class ColorFilter:
         hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
         lower = np.array(rospy.get_param("~lower_hsv", [34.32, 99.91, 107.88]))
         upper = np.array(rospy.get_param("~higher_hsv", [95.625, 255, 255]))
-        mask = cv2.inRange(hsv, lower, upper)    
+        mask = cv2.inRange(hsv, lower, upper)
         return mask
