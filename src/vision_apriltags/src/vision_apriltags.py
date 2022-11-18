180a51bf246942bbf64bd8a3896bbd0c84cc0137
THajiyev <75402235+THajiyev@users.noreply.github.com>
2022-11-17 22:54:09 -0600

    Position on the field
    Untested 11/17

--- src/vision_apriltags/src/vision_apriltags.py
+++ src/vision_apriltags/src/vision_apriltags.py
@@ -8,6 +8,7 @@ import sys
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float32, Float64, Bool
+import json
 
 rospy.init_node('vision_apriltags', anonymous=True)
 
@@ -18,13 +19,10 @@ class VisionApriltags:
             "/apriltags/pose_est", Float32, queue_size=1)
 
         video_dev = int(rospy.get_param("~video_dev", 0))
-        print(video_dev)
         self.cap = cv2.VideoCapture(video_dev + cv2.CAP_V4L)
-        # self.cap = cv2.VideoCapture(0)
         
 
-        self.cap.set(cv2.CAP_PROP_FOURCC,
-                     cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         self.cap.set(cv2.CAP_PROP_FPS, 30.0)
@@ -36,7 +34,10 @@ class VisionApriltags:
         self.cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 2000)
         self.cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
         self.cap.set(cv2.CAP_PROP_FOCUS, 0)
-        
+
+        self.height = 26 
+        self.camera_angle = 33
+        self.data = json.load(open('locations.json'))
         print(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH), self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT), self.cap.get(cv2.CAP_PROP_FPS))
 
         if not self.cap.isOpened():
@@ -53,22 +54,18 @@ class VisionApriltags:
             refine_pose=True,
             debug=False,
             quad_contours=True)
+        tag_size = 6
+
+        self.axis_const = [
+                            [-tag_size / 2, -tag_size / 2, 0], 
+                            [-tag_size / 2, tag_size / 2, 0], 
+                            [tag_size / 2, tag_size / 2, 0], 
+                            [tag_size / 2, -tag_size / 2, 0]
+        ]
 
         self.detector = apriltag.Detector(options)
 
     def main(self):
-        # tag_size = 6.3125
-        tag_size = 6
-
-        axis = np.float32([
-            [-tag_size / 2, -tag_size / 2, 0],
-            [-tag_size / 2, tag_size / 2, 0],
-            [tag_size / 2, tag_size / 2, 0],
-            [tag_size / 2, -tag_size / 2, 0],
-            [-tag_size / 2, -tag_size / 2, tag_size],
-            [-tag_size / 2, tag_size / 2, tag_size],
-            [tag_size / 2, tag_size / 2, tag_size],
-            [tag_size / 2, -tag_size / 2, tag_size]])
 
         with np.load(rospy.get_param("~calibration_file")) as data:
             mtx = data['arr_0']
@@ -97,16 +94,26 @@ class VisionApriltags:
                 # print(r.decision_margin)
                 
                 print(r.corners)
-
-                _, rVec, tVec = cv2.solvePnP(
-                    axis[:4], np.mat(r.corners), mtx, dist)
-
+                id = str(r.tag_id)
+                _, rVec, tVec = cv2.solvePnP(self.get_axis(self.data[id]), np.mat(r.corners), mtx, dist)
                 Rt = np.mat(cv2.Rodrigues(rVec)[0])
                 R = Rt.transpose()
                 pos = -R * tVec
-
+                print(self.data[id]["Location"])
                 print(pos)
                 print(R)
+
+    def get_axis(self, data):
+        axis = []
+        for [x,y,z] in self.axis_const:
+            axis.append(
+                [
+                    x+data['X'],
+                    y+data['Y'],
+                    z+data['Z']-self.height,
+                ]
+            )
+        return np.float32(axis)
 
 
 if __name__ == '__main__':
